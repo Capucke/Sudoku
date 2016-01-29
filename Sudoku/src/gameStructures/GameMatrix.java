0af4c15eedaa08a9e29093a54b8395696887c787
Deborah Leder <deborah.leder@ensimag.grenoble-inp.fr>
2016-01-29 23:24:37 +0100

    GameMatrix : verifGrille() terminée
    verifGrille est une méthode "lente" qui va parcourir toutes les cases et
    marquer les cases mal remplies (= numéro présent 2 fois dans une même ligne,
    colonne ou carré
    
    Amélioration ?
    - garder cette vérif lente et implémenter une vérif rapide qui ne met pas à jour
      les marqueurs des cases

--- Sudoku/src/gameStructures/GameMatrix.java
+++ Sudoku/src/gameStructures/GameMatrix.java
@@ -40,13 +40,9 @@ public class GameMatrix {
 
 	public Case[] getCarre(int indice) {
 		if (indice < 0 || indice >= DIMENSION) {
-			throw new IllegalArgumentException(
-					"La dimension de la grille de jeu vaut : " + DIMENSION
-							+ ". Il n'y a donc que " + DIMENSION
-							+ " \"sous-grilles\" de jeu, indicées entre 0 et "
-							+ (DIMENSION - 1)
-							+ ".\n Vous avez demandé la \"sous-grilles\" de jeu d'indice "
-							+ indice);
+			throw new IllegalArgumentException("La dimension de la grille de jeu vaut : " + DIMENSION
+					+ ". Il n'y a donc que " + DIMENSION + " \"sous-grilles\" de jeu, indicées entre 0 et "
+					+ (DIMENSION - 1) + ".\n Vous avez demandé la \"sous-grilles\" de jeu d'indice " + indice);
 		}
 
 		Case[] carre = new Case[DIMENSION];
@@ -64,9 +60,8 @@ public class GameMatrix {
 		}
 
 		if (k != DIMENSION) {
-			throw new InternalError("Mauvaise initialisation de DIMENSION (="
-					+ DIMENSION + ") et DIM_UNIT (=" + DIM_UNIT + ").\n"
-					+ "On doit avoir : DIM_UNIT = sqrt(DIMENSION)");
+			throw new InternalError("Mauvaise initialisation de DIMENSION (=" + DIMENSION + ") et DIM_UNIT (="
+					+ DIM_UNIT + ").\n" + "On doit avoir : DIM_UNIT = sqrt(DIMENSION)");
 		}
 
 		return carre;
@@ -74,13 +69,9 @@ public class GameMatrix {
 
 	public Case[] getCol(int indice) {
 		if (indice < 0 || indice >= DIMENSION) {
-			throw new IllegalArgumentException(
-					"La dimension de la grille de jeu vaut : " + DIMENSION
-							+ ". Il n'y a donc que " + DIMENSION
-							+ " colonnes dans la grille, indicées entre 0 et "
-							+ (DIMENSION - 1)
-							+ ".\n Vous avez demandé la colonne d'indice "
-							+ indice);
+			throw new IllegalArgumentException("La dimension de la grille de jeu vaut : " + DIMENSION
+					+ ". Il n'y a donc que " + DIMENSION + " colonnes dans la grille, indicées entre 0 et "
+					+ (DIMENSION - 1) + ".\n Vous avez demandé la colonne d'indice " + indice);
 		}
 
 		Case[] col = new Case[DIMENSION];
@@ -94,13 +85,9 @@ public class GameMatrix {
 
 	public Case[] getLine(int indice) {
 		if (indice < 0 || indice >= DIMENSION) {
-			throw new IllegalArgumentException(
-					"La dimension de la grille de jeu vaut : " + DIMENSION
-							+ ". Il n'y a donc que " + DIMENSION
-							+ " lignes dans la grille, indicées entre 0 et "
-							+ (DIMENSION - 1)
-							+ ".\n Vous avez demandé la ligne d'indice "
-							+ indice);
+			throw new IllegalArgumentException("La dimension de la grille de jeu vaut : " + DIMENSION
+					+ ". Il n'y a donc que " + DIMENSION + " lignes dans la grille, indicées entre 0 et "
+					+ (DIMENSION - 1) + ".\n Vous avez demandé la ligne d'indice " + indice);
 		}
 
 		Case[] ligne = new Case[DIMENSION];
@@ -111,11 +98,39 @@ public class GameMatrix {
 
 		return ligne;
 	}
-	
-	public boolean verifGrille(){
-		boolean pbGrille = false;
-		
-		return (!pbGrille);
+
+	public boolean verifGrille() {
+		boolean grilleOK = true;
+		boolean curUnitOk;
+
+		// vérif des lignes
+		for (int ligne = 0; ligne < DIMENSION; ligne++) {
+			curUnitOk = this.verif(this.getLine(ligne));
+			if (!curUnitOk) {
+				grilleOK = false;
+			}
+
+		}
+
+		// vérif des colonnes
+		for (int col = 0; col < DIMENSION; col++) {
+			curUnitOk = this.verif(this.getCol(col));
+			if (!curUnitOk) {
+				grilleOK = false;
+			}
+
+		}
+
+		// vérif des carrés
+		for (int carre = 0; carre < DIMENSION; carre++) {
+			curUnitOk = this.verif(this.getCarre(carre));
+			if (!curUnitOk) {
+				grilleOK = false;
+			}
+
+		}
+
+		return grilleOK;
 	}
 
 	/**
@@ -123,9 +138,8 @@ public class GameMatrix {
 	 * double dans une "unité de jeu" représentée par un tableau de cases. Par
 	 * "unité de jeu", on entend : une ligne, une colonne, ou un carré de taille
 	 * DIMENSION. Selon les doubles trouvés, on marquera les cases de la grille
-	 * en conséquence. On renvoie, de plus, un booléen notFull qui vaut true si
-	 * l'unité de jeu n'est pas entièrement remplie (autrement dit, s'il y a des
-	 * zéro dedans).
+	 * en conséquence. On renvoie, de plus, un booléen unitOk qui vaut true si
+	 * l'unité de jeu est entièrement et correctement remplie.
 	 * 
 	 * @param uniteJeu
 	 *            tableau de cases de taille DIMENSION représentant une
@@ -133,14 +147,11 @@ public class GameMatrix {
 	 */
 	private boolean verif(Case[] uniteJeu) {
 		if (uniteJeu.length != DIMENSION) {
-			throw new InternalError(
-					"Le paramètre passé à verif est mauvais : c'est un tableau de taille "
-							+ uniteJeu.length
-							+ " alors que la dimension de la grille de jeu est "
-							+ DIMENSION);
+			throw new InternalError("Le paramètre passé à verif est mauvais : c'est un tableau de taille "
+					+ uniteJeu.length + " alors que la dimension de la grille de jeu est " + DIMENSION);
 		}
 
-		boolean notFull = false;
+		boolean unitOk = true;
 
 		boolean[] vu1fois = new boolean[DIMENSION];
 		boolean[] vuPlusieursFois = new boolean[DIMENSION];
@@ -148,11 +159,14 @@ public class GameMatrix {
 		int k;
 		int indiceMark;
 
+		// initialisation des tableaux de "déjà vu" à false
 		for (k = 0; k < DIMENSION; k++) {
 			vu1fois[k] = false;
 			vuPlusieursFois[k] = false;
 		}
 
+		// passe 1 :
+		// on "compte" le nombre de fois où on voit chaque numéro non nul
 		for (k = 0; k < DIMENSION; k++) {
 			indiceMark = uniteJeu[k].getNum() - 1;
 
@@ -161,18 +175,22 @@ public class GameMatrix {
 				// le numéro de la case est 0
 				// autrement dit, où l'utilisateur n'a pas encore rempli la case
 
-				notFull = true;
+				unitOk = false;
 
 			} else {
 
 				if (vu1fois[indiceMark] == true) {
 					vuPlusieursFois[indiceMark] = true;
+					unitOk = false;
 				} else {
 					vu1fois[indiceMark] = true;
 				}
 			}
 		}
 
+		// passe 2 :
+		// les cases dont le numéro a été vu plusieurs fois sont marquées
+		// comme "problème", les autres sont marquées comme ok
 		for (k = 0; k < DIMENSION; k++) {
 			indiceMark = uniteJeu[k].getNum() - 1;
 
@@ -188,7 +206,7 @@ public class GameMatrix {
 
 		}
 
-		return notFull;
+		return unitOk;
 
 	}
 
